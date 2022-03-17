9dfe6d6b41da5ee11209217fb9025a78ae6b7361
Athira-C-P <athiracp4422@gmail.com>
2022-03-18 00:40:17 +0530

    Added address book

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -5,17 +5,6 @@ class Contacts {
     private String firstName, lastName, address, city, state, email;
     private Long zip, phoneNumber;
 
-    public Contacts(String firstName, String lastName, String address, String city, String state, Long zip, Long phoneNumber, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
     public String getFirstName() {
         return firstName;
     }
